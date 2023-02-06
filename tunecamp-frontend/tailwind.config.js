be506cb203bbc0d77f8c6cf96a79f2fdb2c9f10b
mi-wit <artakserkses.mw@gmail.com>
2023-02-06 14:10:34 +0100

    add tailwind css

---
+++ tunecamp-frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: '',
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

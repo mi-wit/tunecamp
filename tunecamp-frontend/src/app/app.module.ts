8afd121a2f969d47feaa7cd55d7f2a5a8bb16742
mi-wit <artakserkses.mw@gmail.com>
2023-02-13 14:16:30 +0100

    add welcome-page component

--- tunecamp-frontend/src/app/app.module.ts
+++ tunecamp-frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './views/welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
